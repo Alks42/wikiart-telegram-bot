f9787d3e6fe06cca06864805d60e2370a1d6657e
AR <kadasana.kadasana@gmail.com>
2022-10-25 01:00:30 +0300

    added some safety

--- vercel/index.py
+++ vercel/index.py
@@ -1,9 +1,12 @@
 import bot
 from flask import Flask
+from os import environ
+
+UNIC_URL = '/' + environ['UNIC_URL']
 
 app = Flask(__name__)
 
-@app.route('/unic-indefecator')
+@app.route(UNIC_URL)
 def run_script():
     return bot.main()
 
